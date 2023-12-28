22d63c139215862232f0192ad655d0bd77992e83
Hryhoreieva Iryna <grigoreva108@gmail.com>
2023-12-28 20:28:01 +0200

    создала js

---
+++ js/main.js
@@ -0,0 +1,50 @@
+import{photoDescriptions}from"./data_constans.js"
+ import{comentVariationsList}from"./data_constans.js"
+ import{authorsNamesList}from"./data_constans.js"
+const randomInt=function(min,max){
+    min = Math.ceil(min)
+    max = Math.floor(max) 
+    return Math.floor(Math.random()*(max-min)+min)
+}
+
+const id= function() {
+    return crypto .randomUUID()
+} 
+const creatBulkoComments=function(amountOfComments){
+    const comments=[]
+ 
+for(letcounter=1;counter<amountOfComments;counter++){
+    const comment={
+        "id":"",
+        "avatar":"",
+        "message":"",
+        "name":""
+    }
+coment.id=ID()
+coment.avatar=`../img/avatar-${counter}.svg`
+coment.message=comentVariationsList[randomInt(0,comentVariationsList.lenght)]
+coment.name=authorsNamesList[random(0,authorsNamesList.length)]
+coment.push(coment)
+}
+return comments
+}
+const creatPhotoObjects=function(photoAmountMinusOne){
+    const PhotoObjects=[]
+ 
+for(counter=1;counter<photoAmountMinusOne;counter++){
+    const photoObjects={
+        "id":"",
+        "url":"",
+        "discription":"",
+        "likes":"",
+        "comments":""
+    }
+photoObjects.id=(counter)
+photoObjects.url=`../img/photos-${counter}.jpg`
+photoObjects.description=photoDescriptions[counter]
+photoObjects.likes=random(1,33)
+photoObjects.comments=[...creatBulkoComments(randomInt(1.5))]
+photoObjects.push(PhotoObject)
+}
+return photoObjects
+}
